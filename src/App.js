246fab09e1b6bb51e026d8e5c0b9ecb910ab1619
ashfaque khan <ashfaquek036@gmail.com>
2022-04-13 13:50:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app' >
+<div className="app__header">
+    <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="app__headerImage" />
+</div>
+    </div>
+  )
+}
+
+export default App
